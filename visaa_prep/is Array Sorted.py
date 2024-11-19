0da47ed3a82815487e85d28ead89420e99576364
MRECW463 <raghasrigangaputhra@gmail.com>
2024-11-19 19:53:55 +0530

    Create is Array Sorted.py

---
+++ visaa_prep/is Array Sorted.py
@@ -0,0 +1,8 @@
+def sorted(arr):
+    for i in range(1,len(arr)):
+        if arr[i]<arr[i-1]:
+            return False
+    return True
+n=int(input())
+arr=list(map(int,input().split()))
+print("true" if sorted(arr) else "false")
